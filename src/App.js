6040a8470a223925ea89bd93f62b8407a969779c
shaileshJ <shailesh.junghare@gmail.com>
2025-04-02 18:27:07 +0530

    add https

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   console.log("Helloo")
   const [post, setPosts] = useState([]);
   //this.APIHOSTPORT = `${process.env.NODE_APP_APIHOSTPORT}`;
-  var url = `http://${process.env.NODE_APP_APIHOSTPORT}/api/users`
+  var url = `https://${process.env.NODE_APP_APIHOSTPORT}/api/users`
   useEffect(() => {
     //var url = `http://${this.APIHOSTPORT}/languages/${this.props.id}/vote`
     //axios.get(url)
@@ -45,7 +45,7 @@ function App() {
     //     setPosts(response.json());
     //   })
     //axios.get('http://localhost:3001/api/users')
-    axios.get('http://expressapp-backend.up.railway.app/api/users')
+    axios.get('https://expressapp-backend.up.railway.app/api/users')
       //axios.get('https://jsonplaceholder.typicode.com/posts')
       // .then(response => {
       //   //console.log(response);
